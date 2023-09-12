868d283c2a65338809cdec107e4b663900317882
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-13 00:36:59 +0300

    addRoutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from "./about/about.component";
+import {PostsComponent} from "./posts/posts.component";
+import {HomeComponent} from "./home/home.component";
+import {CreateComponent} from "./posts/create/create.component";
+import {ShowComponent} from "./posts/show/show.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about',component:AboutComponent},
+  {path:'posts',
+  children:[
+    {path:'' ,component:PostsComponent},
+    {path:'create' ,component:CreateComponent},
+    {path:'show' ,component:ShowComponent},
+
+      ]},
+  {path:'home',component:HomeComponent},
+  {path:'**',component:HomeComponent},
+  {path:'',component:HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PostsComponent } from './posts/posts.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './posts/create/create.component';
+import { ShowComponent } from './posts/show/show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    PostsComponent,
+    HeaderComponent,
+    HomeComponent,
+    CreateComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
